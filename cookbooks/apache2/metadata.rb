8bf4d964c4f05ed91300a3f1a488e20b7889b677
Rafael Ferreira <rafa.knoppix@gmail.com>
2014-10-20 02:38:27 -0200

    Criando cookbook apache2 - vaziu

---
+++ cookbooks/apache2/metadata.rb
@@ -0,0 +1,7 @@
+name             'apache2'
+maintainer       'Rafael.com'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved Rafael.com' 
+description      'Installs/Configures apache2'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
